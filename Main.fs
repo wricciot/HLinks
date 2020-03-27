8261571077d47da4e4c94cdf1688fe619607c3bb
wricciot <research@wilmer-ricciotti.net>
2020-03-27 12:25:54 +0000

    Added a small example (showing some bugs...)

--- Main.fs
+++ Main.fs
@@ -7,7 +7,32 @@
  ** author: Wilmer Ricciotti                                                **
  *****************************************************************************)
 
+open Query
+
+let sItems = [("name",()); ("quantity",())] |> Map.ofList 
+let sOrders = [("oid",()); ("item",()); ("quantity",())] |> Map.ofList
+let tbItems = Table ("Items", sItems)
+let tbOrders = Table ("Orders", sOrders)
+
+(*
+  distinct
+    for o <- tbOrders do
+    for i <- tbItems do
+      if (i.quantity < o.quantity) then [{ badorder := o.name}]
+*)
+let qTest = 
+  Dedup 
+    (For (["o", tbOrders],
+      (For (["i", tbItems],
+        If (Apply (Primitive "<", [Project (Var ("o", sOrders), "quantity"); Project (Var ("i", sItems), "quantity")]),
+          Singleton (Record (["badorder", Project (Var ("o", sOrders), "name")] |> Map.ofList)),
+          nil)))))
+
 [<EntryPoint>]
-let main argv =
-    printfn "%A" argv
+let main _argv =
+    printfn "*** printing test query"
+    printfn "%s" (string_of_t qTest)
+    let qTest = norm Map.empty qTest in
+    printfn "*** printing normalized test query"
+    printfn "%s" (string_of_t qTest)
     0 // return an integer exit code
